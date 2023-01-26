7b64a7e96881dea1b381eb19dcc477fde644c438
fahadakhan2 <kanlifahad@gmail.com>
2023-01-26 15:05:44 -0600

    Implemented Transforming the Document collection for Specified Usage
    The TransformDocumentCollection class will take the document collection returned from our previous document collection class. It will than transform the text that is recieved as a string to "tokens" aka words instead of a string. This will be needed to implement the matching and ranking function written before since they use strings split into words rather than just strings to work properly.

--- indexing_process.py
+++ indexing_process.py
@@ -4,10 +4,48 @@ import typing
 
 class DocumentCollection:
     def __init__(self):
-        self.docs = []
+        self.docs: typing.List[Document] = []
 
-    def add_document(self, doc):
+    def add_document(self, doc: Document):
         self.docs.append(doc)
+
+    def get_all_docs(self) -> typing.List[Document]:
+        return self.docs
+
+
+
+class TransformedDocument(typing.NamedTuple):
+    doc_id: str
+    tokens: typing.List[str]
+
+
+class TransformedDocumentCollection:
+    def __init__(self):
+        self.docs: typingList[TransformedDocument] = []
+
+    def add_document(self, doc: TransformedDocument):
+        self.docs.append(doc)
+
+    def write(self, path: str):
+        json_data = {'docs': [td._asdict() for td in self.docs]}
+        with open(path, 'w') as fp:
+            json.dump(obj=json_data, fp=fp)
+
+
+    @staticmethod
+    def read(path:str) -> 'TransformedDocumentCollection':
+        out = TransformedDocumentCollection()
+        with open(path) as fp:
+            collection_dict = json.load(fp)
+
+        doc_records = collection_dict['docs']
+        for record in doc_records:
+            doc = TransformedDocument(doc_id=record['doc_id'], tokens=record['tokens'])
+            out.add_document(doc)
+        return out
+
+
+
 
 
 class Index:
@@ -29,18 +67,28 @@ class WikiSource:
             doc_records = json.load(fp)
         doc_collection = DocumentCollection()
         for record in doc_records:
-            doc_collection.add_document(Document(doc_id=record['id'], text=record['init_text']))
+            doc = Document(doc_id=record['id'], text=record['init_text'])
+            doc_collection.add_document(doc)
         return doc_collection
 
 
-def transform_document(document_text) -> typing.List[str]:
+def tokenize(document_text: str) -> typing.List[str]:
     return document_text.lower().split()
 
 
 
 
 def transform_documents(document_collection):
-    pass
+    docs = document_collection.get_all_docs()
+    out = TransformedDocumentCollection()
+    for d in docs:
+        tokens = tokenize(d.text)
+        transformed_doc = TransformedDocument(doc_id=d.doc_id, tokens=tokens)
+        out.add_document(transformed_doc)
+    return out
+
+
+
 
 
 def create_index(transformed_documents):
@@ -50,5 +98,6 @@ def create_index(transformed_documents):
 def indexing_process(document_source: WikiSource) -> (DocumentCollection, Index):
     document_collection = document_source.read_documents()
     transformed_documents = transform_documents(document_collection)
+    # transformed_documents.write(path ='')
     index = create_index(transformed_documents)
     return (document_collection, index)
